07b33bda722215934715cd246346d4af8554e028
sashen <sashen.moodley@sybrin.com>
2020-05-08 09:35:47 +0200

    as

--- FlateOrchestrationLayer/Controllers/ValuesController.cs
+++ FlateOrchestrationLayer/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace FlateOrchestrationLayer.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value11", "value21" };
+            return new string[] { "value111111", "value211111" };
         }
 
         // GET api/values/5
